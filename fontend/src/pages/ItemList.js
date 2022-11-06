e39ed6b50e147509e13bcc91f9ad59f4b36a7bd0
elkhfad <fadi.el-khouri@hotmail.com>
2022-11-06 14:08:04 +0200

    edit search input

--- fontend/src/pages/ItemList.js
+++ fontend/src/pages/ItemList.js
@@ -7,21 +7,20 @@ import Pagination from '../component/Pagination';
 import { useState } from 'react';
 import Items from '../component/Items';
 
-
 const ItemList = () => {
   const url = 'http://localhost:3001/api/items';
   const { data, error, isPending, setData } = useGetAllItems(url);
   const [search, setSearch] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage,setPostPerPage] = useState(12);
+  const [postsPerPage, setPostPerPage] = useState(12);
   const results = data.filter((result) => {
     return result.title.includes(search);
   });
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = results.slice(indexOfFirstPost, indexOfLastPost);
-  const pageSizes = [3, 6, 9,12,20,50,100];
-  const paginate = pageNumber => {
+  const pageSizes = [3, 6, 9, 12, 20, 50, 100];
+  const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
   const handleSearch = (event) => {
@@ -42,14 +41,14 @@ const ItemList = () => {
         <CreateItem data={data} setData={setData} />
       </div>
       <div className="container mt-5">
-      <h1 className="text-primary mn-3">My Items</h1>
-      <div className="search">
-        <input  name="search" placeholder='search by title' onChange={handleSearch} value={search} />
-      </div>
-      <Items data={currentPosts} isPending={isPending}currentPage={currentPage} />
-     
-      <div className="mt-3">
-          {"Items per Page: "}
+        <h1 className="text-primary mn-3">My Items</h1>
+        <div className="search">
+          <input id="search" name="search" placeholder="search by title" onChange={handleSearch} value={search} />
+        </div>
+        <Items data={currentPosts} isPending={isPending} currentPage={currentPage} />
+
+        <div className="mt-3">
+          {'Items per Page: '}
           <select onChange={handlePageSizeChange} value={postsPerPage}>
             {pageSizes.map((size) => (
               <option key={size} value={size}>
@@ -58,16 +57,9 @@ const ItemList = () => {
             ))}
           </select>
 
-          <Pagination
-        paginate={paginate}
-        postsPerPage={postsPerPage}
-        totalPosts={results.length}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      />
+          <Pagination paginate={paginate} postsPerPage={postsPerPage} totalPosts={results.length} setCurrentPage={setCurrentPage} currentPage={currentPage} />
         </div>
-    
-    </div>
+      </div>
     </div>
   );
 };
