9736e37d17786c50155f3f039792d6ac3325f728
elkhfad <fadi.el-khouri@hotmail.com>
2022-11-19 09:12:19 +0200

    fixing the spelling

--- backend/routers/itemrouter.js
+++ backend/routers/itemrouter.js
@@ -30,102 +30,103 @@ itemsRouter.get('/all', (request, response) => {
   Item.find().then((items) => {
     response.json(items);
   });
-  itemsRouter.get('/:id', async (request, response, next) => {
-    const token = getToken.getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const user = await User.findById(decodedToken.id);
-    const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
+});
+itemsRouter.get('/:id', async (request, response, next) => {
+  const token = getToken.getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const user = await User.findById(decodedToken.id);
+  const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
 
-    if (!token || !decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' });
-    } else if (findItem) {
-      try {
-        const item = await Item.findById(request.params.id);
-        if (item) {
-          response.json(item);
-        } else {
-          response.status(404).end();
-        }
-      } catch (exception) {
-        next(exception);
-      }
-    } else {
-      return response.status(401).json({ error: 'unauthorized' });
-    }
-  });
-
-  itemsRouter.delete('/:id', async (request, response, next) => {
-    const token = getToken.getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const user = await User.findById(decodedToken.id);
-    const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
-
-    if (!token || !decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' });
-    } else if (findItem) {
-      try {
-        user.items = user.items.filter((item) => {
-          return item._id.valueOf() !== request.params.id;
-        });
-        await user.save();
-        await Item.findByIdAndDelete(request.params.id);
-        response.status(204).end();
-      } catch (exception) {
-        next(exception);
-      }
-    }
-  });
-
-  itemsRouter.put('/:id', async (request, response, next) => {
-    const token = getToken.getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const user = await User.findById(decodedToken.id);
-    const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
-    if (!token || !decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' });
-    } else if (findItem) {
-      try {
-        const item = await Item.findByIdAndUpdate(request.params.id, request.body);
-        if (item) {
-          response.json(item);
-        } else {
-          response.status(404).end();
-        }
-      } catch (exception) {
-        next(exception);
-      }
-    } else {
-      return response.status(401).json({ error: 'unauthorized' });
-    }
-  });
-
-  itemsRouter.post('/', async (request, response, next) => {
-    const body = request.body;
-    const token = getToken.getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-
-    if (!token || !decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' });
-    }
-    const user = await User.findById(decodedToken.id);
-
-    const item = new Item({
-      title: body.title,
-      comment: body.comment,
-      price: body.price,
-      amount: body.amount,
-      pic: body.pic,
-      user: user._id,
-    });
-
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  } else if (findItem) {
     try {
-      const savedItem = await item.save();
-      user.items = user.items.concat(savedItem._id);
-      await user.save();
-      response.json(savedItem);
+      const item = await Item.findById(request.params.id);
+      if (item) {
+        response.json(item);
+      } else {
+        response.status(404).end();
+      }
     } catch (exception) {
       next(exception);
     }
-  });
+  } else {
+    return response.status(401).json({ error: 'unauthorized' });
+  }
 });
+
+itemsRouter.delete('/:id', async (request, response, next) => {
+  const token = getToken.getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const user = await User.findById(decodedToken.id);
+  const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  } else if (findItem) {
+    try {
+      user.items = user.items.filter((item) => {
+        return item._id.valueOf() !== request.params.id;
+      });
+      await user.save();
+      await Item.findByIdAndDelete(request.params.id);
+      response.status(204).end();
+    } catch (exception) {
+      next(exception);
+    }
+  }
+});
+
+itemsRouter.put('/:id', async (request, response, next) => {
+  const token = getToken.getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const user = await User.findById(decodedToken.id);
+  const findItem = user.items.find((item) => item._id.valueOf() === request.params.id);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  } else if (findItem) {
+    try {
+      const item = await Item.findByIdAndUpdate(request.params.id, request.body);
+      if (item) {
+        response.json(item);
+      } else {
+        response.status(404).end();
+      }
+    } catch (exception) {
+      next(exception);
+    }
+  } else {
+    return response.status(401).json({ error: 'unauthorized' });
+  }
+});
+
+itemsRouter.post('/', async (request, response, next) => {
+  const body = request.body;
+  const token = getToken.getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const item = new Item({
+    title: body.title,
+    comment: body.comment,
+    price: body.price,
+    amount: body.amount,
+    pic: body.pic,
+    user: user._id,
+  });
+
+  try {
+    const savedItem = await item.save();
+    user.items = user.items.concat(savedItem._id);
+    await user.save();
+    response.json(savedItem);
+  } catch (exception) {
+    next(exception);
+  }
+});
+
 module.exports = itemsRouter;
