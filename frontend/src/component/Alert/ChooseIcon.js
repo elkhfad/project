a50f256242e1d1a24550ee18101dbd6c6c965f0b
elkhfad <fadi.el-khouri@hotmail.com>
2022-11-22 19:35:16 +0200

    react leaflet map

--- frontend/src/component/Alert/ChooseIcon.js
+++ frontend/src/component/Alert/ChooseIcon.js
@@ -3,19 +3,19 @@ import { AiOutlineClose } from 'react-icons/ai';
 const ChooseIcon = ({ value, min }) => {
   if (value.length > 0 && value.length < min) {
     return (
-      <div className="danger" style={{ textAlign: 'center' }}>
+      <div className="danger" style={{ textAlign: 'center', fontSize: '1.5em' }}>
         <AiOutlineClose />
       </div>
     );
   } else if (value.length > min) {
     return (
-      <div className="success">
+      <div className="success" style={{ fontSize: '1.5em' }}>
         <AiOutlineCheck />
       </div>
     );
   } else if (value.length !== 0) {
     return (
-      <div className="success">
+      <div className="success" style={{ fontSize: '1.5em' }}>
         <AiOutlineCheck />
       </div>
     );
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -5,12 +5,18 @@ const Contact = () => {
 
   return (
     <div>
-      Hello
-      <MapContainer center={positionValue} zoom={15} scrollWheelZoom={false} style={{ height: '40vh', width: '40vh', margin: '0 auto', marginTop: '10em' }}>
+      <div className="contact">
+        <div className="contactHeader">contact leebstore developer team</div>
+        <div className="contactEmail">via email: Selim.Bedir@metropolia.fi or fadi.el-khouri@metropolia.fi</div>
+        <div className="contactAddress">
+          address: metropolia university of applied sciences <br /> Karaportti 2, 02610 Espoo
+        </div>
+      </div>
+      <MapContainer center={positionValue} zoom={15} scrollWheelZoom={false} style={{ height: '60vh', width: '60vh', margin: '0 auto', marginTop: '10em' }}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={positionValue}>
           <Tooltip permanent>
-            Karaportti 2,
+            Metropolia Karaportti 2,
             <br />
             02610 Espoo
           </Tooltip>
