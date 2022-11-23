33bdb6877695b9b415518b8cd6c6d0448f00d9c2
elkhfad <fadi.el-khouri@hotmail.com>
2022-11-23 10:21:46 +0200

    fixed bad on shopping list

--- frontend/src/component/modal/Cart.js
+++ frontend/src/component/modal/Cart.js
@@ -6,10 +6,16 @@ import { IoReturnDownBackOutline } from 'react-icons/io5';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 import AlertComponent from '../Alert/AlertComponent';
+import Confirm from './Confirm';
+import { BsTrash } from 'react-icons/bs';
+import moment from 'moment';
+import { useGetCartList } from '../control/useGetCardList';
 
 const Cart = () => {
   const navigate = useNavigate();
+  const cartUrl = 'http://localhost:3001/api/carts/all';
   const [isPending, setIsPending] = useState(true);
+  const { cartdata } = useGetCartList(cartUrl);
   const [error, setError] = useState(null);
   const { id } = useParams();
   const [carts, setCart] = useState([]);
@@ -45,6 +51,18 @@ const Cart = () => {
         setError(err.message);
       });
   };
+  const handleDelete = (id) => {
+    services.deleteCart(url, id).then((res) => {
+      navigate('/');
+    });
+  };
+  const handleTime = () => {
+    const cart = cartdata.filter((cart) => {
+      return cart.id === id;
+    });
+    const time = cart[0]?.time;
+    return time;
+  };
 
   const sum = () => {
     let sum = 0;
@@ -58,9 +76,25 @@ const Cart = () => {
       <div>{isPending && <Spinner animation="border" variant="primary" />}</div>
       <div>{error && <AlertComponent variant="danger" header="You got an error!" text={error} />}</div>
       <div>
-        <Button style={{ float: 'right', marginRight: '2em' }} className="returnToList" onClick={() => navigate('/cartList')}>
+        <Button style={{ float: 'right', marginRight: '2em' }} className="returnToList" onClick={() => navigate('/')}>
           Back <IoReturnDownBackOutline />
         </Button>
+      </div>
+      <div className="cartDate">cart created {moment(new Date(handleTime())).format('DD/MM/YYYY hh:mm:ss')}</div>
+      <div style={{ float: 'left', margin: '1em' }}>
+        <Confirm
+          icon={<BsTrash />}
+          title={`Are you sure ?`}
+          body={`You won't be able to revert deleted item!`}
+          confirm="Yes delete it"
+          cancelColor="success"
+          confirmColor="danger"
+          buttonName="Delete"
+          itemDeleteBtn="cartDeleteBtn"
+          handleClick={() => {
+            handleDelete(id);
+          }}
+        />
       </div>
       <Table striped bordered hover>
         <thead>
@@ -94,7 +128,6 @@ const Cart = () => {
           </tr>
         </tbody>
       </Table>
-
       <Button className="buyCart" onClick={() => handleBuy()}>
         Buy
       </Button>
--- frontend/src/component/modal/cartsList.js
+++ frontend/src/component/modal/cartsList.js
@@ -1,67 +1,25 @@
 import { useGetCartList } from '../control/useGetCardList';
 import { useNavigate } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import moment from 'moment';
-import Spinner from 'react-bootstrap/Spinner';
-import { IoReturnDownBackOutline } from 'react-icons/io5';
-import { BsCart4 } from 'react-icons/bs';
-import services from '../../services/cartsService';
-import Confirm from './Confirm';
-import { BsTrash } from 'react-icons/bs';
+import { useEffect } from 'react';
 
 const CartList = () => {
   const cartUrl = 'http://localhost:3001/api/carts/all';
-  const url = 'http://localhost:3001/api/carts';
-  const { cartdata, isPending } = useGetCartList(cartUrl);
+  const { cartdata } = useGetCartList(cartUrl);
   const navigate = useNavigate();
-
-  const seeList = (id) => {
-    navigate(`${id}`);
-  };
-  const handleDelete = (id) => {
-    services.deleteCart(url, id).then((res) => {
-      navigate('/');
+  useEffect(() => {
+    cartdata.map((d, index) => {
+      if (d.wish === true) {
+        return (
+          <div key={d.id + index} className="cartStyle">
+            {navigate(`${d.id}`)}
+          </div>
+        );
+      }
+      return '';
     });
-  };
-  return (
-    <div>
-      <div>{isPending && <Spinner animation="border" variant="primary" />}</div>
-      <div>
-        <Button style={{ float: 'right', marginRight: '4em', marginTop: '1em' }} className="returnToList" onClick={() => navigate('/')}>
-          Back <IoReturnDownBackOutline />
-        </Button>
-      </div>
-      <br />
-      <div className="cartList">
-        Cart
-        {cartdata.map((d, index) => {
-          if (d.wish === true) {
-            return (
-              <div key={d.id + index} className="cartStyle">
-                <Button onClick={() => seeList(d.id)} className="cartBtn">
-                  cart created {moment(new Date(d.time)).format('DD/MM/YYYY hh:mm:ss')}
-                </Button>
-                <Confirm
-                  icon={<BsTrash />}
-                  title={`Are you sure ?`}
-                  body={`You won't be able to revert deleted item!`}
-                  confirm="Yes delete it"
-                  cancelColor="success"
-                  confirmColor="danger"
-                  buttonName="Delete"
-                  itemDeleteBtn="cartDeleteBtn"
-                  handleClick={() => {
-                    handleDelete(d.id);
-                  }}
-                />
-              </div>
-            );
-          }
-          return '';
-        })}
-      </div>
-    </div>
-  );
+  }, [cartdata, navigate]);
+
+  return <div></div>;
 };
 
 export default CartList;
