d99c11e54a8b2f9f3b9a06831af48573d319b89b
elkhfad <fadi.el-khouri@hotmail.com>
2022-11-24 20:57:54 +0200

    adding euro icon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,4 +5,12 @@ import './styles/App.css';
 import './styles/styleMedia.css';
 import './styles/itemlist.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL_PROD;
+} else if (process.env.NODE_ENV === 'test') {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL_TEST;
+} else {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL_DEV;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
